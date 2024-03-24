ef0d995624d260af2ba79730478c7c5763f77c57
vorn-dev-ni <Nightpp19@gmail.com>
2024-03-24 14:45:08 +0700

    refactor link job api project

--- src/components/home/Project.tsx
+++ src/components/home/Project.tsx
@@ -119,7 +119,7 @@ const initState = [
     tag: ["All Project", "Back End"],
     description: "Job Application API similar to linkedIn ",
     languages: [skills[8], skills[9]],
-    link: "",
+    link: "https://main--panhavorn-portfolio.netlify.app/",
     sourceCode: "https://gitlab.com/Vornii/jobapplicationapi",
   },
   {
